17aaee8bfd5a4a2fa0782aa6ad223d6c75e82dc4
Mouadhbendjedidi <alfadjr2007@gmail.com>
2025-06-03 21:49:09 +0100

    added socat for wsl and alias for cord.nvim

--- home.nix
+++ home.nix
@@ -33,6 +33,7 @@
     pkgs.lazygit
     pkgs.rustup
     pkgs.cargo-watch
+    pkgs.socat
   ];
 
   home.file = {
@@ -76,6 +77,19 @@
 
       eval "$(fzf --zsh)"
       eval "$(zoxide init --cmd cd zsh)"
+      nvim() {
+        if ! pidof socat > /dev/null 2>&1; then
+          [ -e /tmp/discord-ipc-0 ] && rm -f /tmp/discord-ipc-0
+          socat UNIX-LISTEN:/tmp/discord-ipc-0,fork \
+              EXEC:"npiperelay.exe //./pipe/discord-ipc-0" 2>/dev/null &
+        fi
+
+        if [ $# -eq 0 ]; then
+            command nvim
+        else
+            command nvim "$@"
+        fi
+        }
     '';
     plugins = [
       {
